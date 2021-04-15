71df547eea0ae09b20a279ebfc5396debd71697a
Penghe Zhang <ly20254@bristol.ac.uk>
2021-04-15 15:22:02 +0100

    add background music part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Guide to the Solar system';
+  bgm = new Audio('assets/bgm.mp3');
+  constructor( private router: Router) {
+    this.playMusic();
+  }
+
+  redirectTo(where: string): void {
+    console.log('redir');
+    this.router.navigate([where]);
+  }
+
+  playMusic(): void{
+    this.bgm.load();
+    this.bgm.play();
+  }
+  stopMusic(): void{
+    this.bgm.pause();
+  }
 }
